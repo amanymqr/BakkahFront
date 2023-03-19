af0b2e27bf854ce5eee312f2a0dbe4c71559532d
amanymqr <a.moqr20@gmail.com>
2023-03-19 21:03:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(window).scroll(function() {
+    let top = $(window).scrollTop();
+
+    if (top > 300) {
+        $('.top').fadeIn();
+    }else {
+        $('.top').fadeOut();
+    }
+})
+
+
+$('.top').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+});
+
